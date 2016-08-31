3e255da072d58b564324f1b45ce0e46473be29ac
js.velasquez2463 <js.velasquez2463@ISIS2503-59.sis.virtual.uniandes.edu.co>
2016-08-31 01:13:00 -0500

    Modificaciones proyecto y clases de campo

--- src/main/java/dto/CampoDTO.java
+++ src/main/java/dto/CampoDTO.java
@@ -1,119 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package dto;
 
+import java.util.List;
+
 /**
  *
- * @author Mauricio
+ * @author Johan
  */
 public class CampoDTO {
  
-    private String name;
+    private Long id;
+
+    private String nombre;
     
-    private String surname;
+    private String ciudad;
+        
+
     
-    private int age;
+    private List<PozoDTO>pozos;
+
     
-    private String telephone;
-    
-    private String cellphone;
-    
-    private String address;
-    
-    private String city;
-    
-    private String country;
-    
-    private String username;
-    
-    private String password;
     
     public CampoDTO(){
         
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public String getCellphone() {
-        return cellphone;
-    }
-
-    public void setCellphone(String cellphone) {
-        this.cellphone = cellphone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
     
-    
-    public String getUsername() {
-        return username;
+    public Long getId() {
+        return id;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public String getPassword() {
-        return password;
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+      
+
+    public List<PozoDTO> getPozos() {
+        return pozos;
+    }
+
+    public void setPozos(List<PozoDTO> pozos) {
+        this.pozos = pozos;
+    }
+   
 }
--- src/main/java/dto/SensorDTO.java
+++ src/main/java/dto/SensorDTO.java
@@ -6,114 +6,50 @@
 
 package dto;
 
-/**
- *
- * @author Mauricio
- */
-public class SensorDTO {
+public class SensorDTO{
+
+    private Long id;
+
+    private String tipo;
+    
+    private double estado;
+    
+    private double limiteNormal;
+
+    
  
-    private String name;
-    
-    private String surname;
-    
-    private int age;
-    
-    private String telephone;
-    
-    private String cellphone;
-    
-    private String address;
-    
-    private String city;
-    
-    private String country;
-    
-    private String username;
-    
-    private String password;
-    
     public SensorDTO(){
         
     }
-
-    public String getName() {
-        return name;
+    
+    public Long getId() {
+        return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public String getSurname() {
-        return surname;
+    public String getTipo() {
+        return tipo;
     }
 
-    public void setSurname(String surname) {
-        this.surname = surname;
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+ public double getEstado() {
+        return estado;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public String getCellphone() {
-        return cellphone;
-    }
-
-    public void setCellphone(String cellphone) {
-        this.cellphone = cellphone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
+    public void setEstado(double estado) {
+        this.estado = estado;
     }
     
-    
-    public String getUsername() {
-        return username;
+    public double getLimiteNormal() {
+        return limiteNormal;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public void setLimiteNormal(double limiteNormal) {
+        this.limiteNormal = limiteNormal;
     }
 }
